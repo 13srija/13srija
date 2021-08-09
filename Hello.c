d6b36e769e3091554c9c6e80f9ca33710a27d2cb
13srija <88663043+13srija@users.noreply.github.com>
2021-08-09 17:42:09 +0530

    Srija info.c
    🙂Nice to see you 
    😊How are  you doing
    ❔What's your name

---
+++ Hello.c
@@ -0,0 +1,10 @@
+- 👋 Hi, I’m @13srija
+- 👀 I’m interested in reading
+- 🌱 I’m currently learning CSE
+- 💞️ I’m looking to collaborate on KL University
+- 📫 How to reach me in whatsapp
+
+<!---
+13srija/13srija is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
